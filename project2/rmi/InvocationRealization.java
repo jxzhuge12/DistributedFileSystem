d86df94747a6bd92f089bc059dcaf5e3a364b156
Xie <weiweichenxie@gmail.com>
2016-05-09 22:06:54 -0700

    start project2

---
+++ project2/rmi/InvocationRealization.java
@@ -0,0 +1,68 @@
+package rmi;
+
+import test.*;
+import java.net.*;
+import java.io.*;
+import java.lang.reflect.*;
+import java.lang.reflect.Proxy;
+
+public class InvocationRealization implements InvocationHandler, Serializable
+{
+    public InetSocketAddress address;
+    
+    public Class c;
+    
+    public InvocationRealization(InetSocketAddress address, Class c)
+    {
+        this.address = address;
+        this.c = c;
+    }
+    
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
+        if(method.getName().equals("equals") && args.length == 1 && method.getParameterTypes()[0].getName() == "java.lang.Object")
+        {
+            if(args[0] == null) return false;
+            if(!Proxy.isProxyClass(args[0].getClass())) return false;
+            InvocationRealization ih = (InvocationRealization)Proxy.getInvocationHandler(args[0]);
+            return (Object) (c.equals(ih.c) && address.getAddress().equals(ih.address.getAddress()) && address.getPort() == ih.address.getPort());
+        }
+        else if(method.getName().equals("hashCode") && args == null) return (Object) ((17 + c.hashCode()) * 13 + address.hashCode());
+        else if(method.getName().equals("toString") && args == null) return (Object) (c.toString() + address.toString());
+        else
+        {
+            Object ret;
+            boolean suc;
+            
+            try
+            {
+                Socket socket = new Socket(address.getAddress(), address.getPort());
+                ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+                String methodString = method.getName();
+                Class<?>[] argTypes = method.getParameterTypes();
+                for (Class<?> argType : argTypes) methodString += argType.getName();
+                outStream.writeObject((Object)methodString.hashCode());
+                outStream.flush();
+                if(args != null)
+                {
+                    for(Object arg : args)
+                    {
+                        outStream.writeObject(arg);
+                        outStream.flush();
+                    }
+                }
+                ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+                suc = (boolean)inStream.readObject();
+                ret = inStream.readObject();
+                socket.close();
+            }
+            catch(Throwable e)
+            { 
+                throw new RMIException("error"); 
+            }
+            
+            if(suc) return ret;
+            else throw ((InvocationTargetException)ret).getTargetException();
+        }
+    } 
+}
